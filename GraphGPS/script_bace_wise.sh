f86a07e204acbf2982f710743c10d5ee4a4f34bf
sallylsk <73758841+sallylsk@users.noreply.github.com>
2025-12-04 08:09:05 -0500

    Add files via upload

---
+++ GraphGPS/script_bace_wise.sh
@@ -0,0 +1,17 @@
+CUDA_VISIBLE_DEVICES=5 python wise_ft.py --cfg configs/GPS/bace_ft_full.yaml --repeat 1 scaffold False 50 0.1
+CUDA_VISIBLE_DEVICES=5 python wise_ft.py --cfg configs/GPS/bace_ft_full.yaml --repeat 1 scaffold False 50 0.3
+CUDA_VISIBLE_DEVICES=5 python wise_ft.py --cfg configs/GPS/bace_ft_full.yaml --repeat 1 scaffold False 50 0.5
+CUDA_VISIBLE_DEVICES=5 python wise_ft.py --cfg configs/GPS/bace_ft_full.yaml --repeat 1 scaffold False 50 0.7
+CUDA_VISIBLE_DEVICES=5 python wise_ft.py --cfg configs/GPS/bace_ft_full.yaml --repeat 1 scaffold False 50 0.9
+
+CUDA_VISIBLE_DEVICES=5 python wise_ft.py --cfg configs/GPS/bace_ft_full.yaml --repeat 1 size False 50 0.1
+CUDA_VISIBLE_DEVICES=5 python wise_ft.py --cfg configs/GPS/bace_ft_full.yaml --repeat 1 size False 50 0.3
+CUDA_VISIBLE_DEVICES=5 python wise_ft.py --cfg configs/GPS/bace_ft_full.yaml --repeat 1 size False 50 0.5
+CUDA_VISIBLE_DEVICES=5 python wise_ft.py --cfg configs/GPS/bace_ft_full.yaml --repeat 1 size False 50 0.7
+CUDA_VISIBLE_DEVICES=5 python wise_ft.py --cfg configs/GPS/bace_ft_full.yaml --repeat 1 size False 50 0.9
+
+CUDA_VISIBLE_DEVICES=5 python wise_ft.py --cfg configs/GPS/bace_ft_full.yaml --repeat 1 random False 50 0.1
+CUDA_VISIBLE_DEVICES=5 python wise_ft.py --cfg configs/GPS/bace_ft_full.yaml --repeat 1 random False 50 0.3
+CUDA_VISIBLE_DEVICES=5 python wise_ft.py --cfg configs/GPS/bace_ft_full.yaml --repeat 1 random False 50 0.5
+CUDA_VISIBLE_DEVICES=5 python wise_ft.py --cfg configs/GPS/bace_ft_full.yaml --repeat 1 random False 50 0.7
+CUDA_VISIBLE_DEVICES=5 python wise_ft.py --cfg configs/GPS/bace_ft_full.yaml --repeat 1 random False 50 0.9
